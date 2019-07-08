f60d66331091ad2be1036b9df12026d9aec4aed1
Shirley Nelson <shirleyn@vbopenshift.s2.abcm.com>
2019-07-08 07:43:54 -0400

    Install Atom

---
+++ install_atom.sh
@@ -0,0 +1,5 @@
+ echo -e "[Atom]\nname=Atom Editor\nbaseurl=https://packagecloud.io/AtomEditor/a>tom/el/7/\$basearch\nenabled=1\ngpgcheck=0\nrepo_gpgcheck=1\ngpgkey=https://pac>kagecloud.io/AtomEditor/atom/gpgkey" > /etc/yum.repos.d/atom.repo
+yum provides */libgconf*
+yum install GConf2
+yum install atom
+atom
