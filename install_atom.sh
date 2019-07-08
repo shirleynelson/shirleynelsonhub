eaef64a0d7877fac1db01b6c9ea9a290706c3f65
Shirley Nelson <shirleyn@vbopenshift.s2.abcm.com>
2019-07-08 09:48:20 -0400

    fix atom

--- install_atom.sh
+++ install_atom.sh
@@ -1,5 +1,8 @@
- echo -e "[Atom]\nname=Atom Editor\nbaseurl=https://packagecloud.io/AtomEditor/a>tom/el/7/\$basearch\nenabled=1\ngpgcheck=0\nrepo_gpgcheck=1\ngpgkey=https://pac>kagecloud.io/AtomEditor/atom/gpgkey" > /etc/yum.repos.d/atom.repo
+echo -e "[Atom]\nname=Atom Editor\nbaseurl=https://packagecloud.io/AtomEditor/a>tom/el/7/\$basearch\nenabled=1\ngpgcheck=0\nrepo_gpgcheck=1\ngpgkey=https://pac>kagecloud.io/AtomEditor/atom/gpgkey" > /etc/yum.repos.d/atom.repo
 yum provides */libgconf*
 yum install GConf2
+yum install libxkbfile
 yum install atom
-atom
+cp init.coffee ~/.atom 
+touch test.html
+atom test.html
